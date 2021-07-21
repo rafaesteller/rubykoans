71098114bc9eb5a6721335a5e4478c32eff0c2ae
Rafael Esteller <esteller.rafa@gmail.com>
2021-07-21 18:04:32 +0200

    progressing

--- about_nil.rb
+++ about_nil.rb
@@ -8,12 +8,12 @@ class AboutNil < Neo::Koan
   def test_you_dont_get_null_pointer_errors_when_calling_methods_on_nil
     # What happens when you call a method that doesn't exist.  The
     # following begin/rescue/end code block captures the exception and
-    # makes some assertions about it.    
+    # makes some assertions about it.
     begin
-      nil.some_method_nil_doesnt_know_about
+      some_method_nil_doesnt_know_about
     rescue Exception => ex
-      # What exception has been caught?      
-      assert_equal NoMethodError, ex.class
+      # What exception has been caught?
+      assert_equal NameError, ex.class
       puts ex.message
       # What message was attached to the exception?
       # (HINT: replace __ with part of the error message.)
--- triangle.rb
+++ triangle.rb
@@ -14,7 +14,30 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  sides_must_add_up_at_least_as_the_hypotenuse(a, b, c)
+  sides_cannot_be_zero(a, b, c)
+  sides_cannot_be_negative(a, b, c)
+  return :equilateral if a == b && b == c
+  return :isosceles if (a != b && b == c) || (a != b && a == c) || (a == b && b != c)
+
+  :scalene
+end
+
+def sides_cannot_be_zero(a, b, c)
+  if(a == 0 || b == 0 || c == 0 )
+    raise TriangleError, "Sides cannot be zero"
+  end
+end
+
+def sides_cannot_be_negative(a, b, c)
+  if(a < 0 || b < 0 || c < 0 )
+    raise TriangleError, "Sides cannot be negative"
+  end
+end
+
+def sides_must_add_up_at_least_as_the_hypotenuse(a, b, c)
+  x, y, z = [a, b, c].sort
+  raise TriangleError if x + y <= z
 end
 
 # Error class used in part 2.  No need to change this code.
